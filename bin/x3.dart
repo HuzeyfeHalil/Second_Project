6cc895828cd88e303770bacac76ca6bc987266a4
HuzeyfeHalil <117576180+HuzeyfeHalil@users.noreply.github.com>
2022-11-17 08:12:14 +0300

    first commit

--- bin/x3.dart
+++ bin/x3.dart
@@ -1,5 +1,15 @@
-import 'package:x3/x3.dart' as x3;
-
-void main(List<String> arguments) {
-  print('Hello world: ${x3.calculate()}!');
+void main() {
+  int x;
+  int c;
+  for (x = 0; x <= 100; x++) {
+    if (x % 2 == 0) ;
+    {
+      for (c = 0; c <= 100; c++) {
+        if (c % 2 == 1) ;
+        {
+          print(c + x);
+        }
+      }
+    }
+  }
 }
